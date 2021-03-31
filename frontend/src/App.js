051b6d20301ef4522e89a35ed3f16ca7557ca2d6
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-31 09:03:25 -0400

    created App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,64 @@ import './App.css';
 import { useState, useEffect, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect, useHistory } from 'react-router-dom'; 
 
+// MUI
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 // Components
 import Navbar from './components/layout/Navbar';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#B7D4DB",
+      main: "#A5CAD2",
+      dark: '#84A1A8',
+      contrastText: "#333"
+    },
+    secondary: {
+      light: "#FF94A1",
+      main: "#FF7A89",
+      dark: "#CC616E",
+      contrastText: "#333"
+    }
+  },
+  spacing: [0, 4, 8, 16, 32, 64]
+})
+
+const Routing = (props) => {
+  const history = useHistory();
+  const { user, getUser, setUser } = props;
+
+  useEffect(() => {
+    if(user){
+      history.push('/');
+    } else {
+      history.push('/login');
+    }
+  }, [user])
+
+  return (
+    <Switch>
+      <Route>
+        {/* <Home>
+          
+        </Home> */}
+      </Route>
+    </Switch>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      Welcome to pictogram
-      <Navbar />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div className="app">
+          <Navbar />
+          <Routing />
+        </div>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
